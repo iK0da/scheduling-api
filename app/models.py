b85353a74a05d4f0641665c2b9ccbbfdad747eee
Yan iK0da <kodadeveloper@proton.me>
2025-07-11 05:50:32 +0100

    refactor(models): remove campos de data do modelo AppointmentRequest

--- app/models.py
+++ app/models.py
@@ -2,10 +2,5 @@ from pydantic import BaseModel
 from typing import Literal
 
 class AppointmentRequest(BaseModel):
-    year: int
-    month: int
-    day: int
-    hour: int
-    minute: int
     service: Literal["extensão", "manutenção", "remoção"]
     client_name: str
